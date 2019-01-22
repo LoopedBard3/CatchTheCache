128c22a710326b5d4ef593d53b516e22e9ea1665
pbibus <pbibus@iastate.edu>
2019-01-22 18:20:46 +0000

    Testing the Gitlab integration

--- Experiments/testing_git_pls_ignore.java
+++ Experiments/testing_git_pls_ignore.java
@@ -3,6 +3,7 @@ package experiments.aidans;
  * @author Aidan Sherburne
  * 
  * A quick test of eclipse git integration
+ * A quick test of the discord integration
  */
 
 public class testing_git_pls_ignore {

1ef6bc1e412353ba932651793b51d596004b6817
aidans <aidans@iastate.edu>
2019-01-23 19:04:19 -0600

    Added existing files
    Added files for springboot initializr experiment

---
+++ Experiments/aidans_experiments/springboot_mysql_test/src/main/java/edu/iastate/cs309/team_4/springboot_mysql_test/SpringbootMysqlTestApplication.java
@@ -0,0 +1,14 @@
+package edu.iastate.cs309.team_4.springboot_mysql_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMysqlTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMysqlTestApplication.class, args);
+	}
+
+}
+
---
+++ Experiments/aidans_experiments/springboot_mysql_test/src/test/java/edu/iastate/cs309/team_4/springboot_mysql_test/SpringbootMysqlTestApplicationTests.java
@@ -0,0 +1,17 @@
+package edu.iastate.cs309.team_4.springboot_mysql_test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootMysqlTestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+

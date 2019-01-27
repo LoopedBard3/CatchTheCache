e978a661adc012beb657afff50e72ce8c49210ff
aidans <aidans@iastate.edu>
2019-01-27 13:18:40 -0600

    Add Spring Boot login example
    Followed a tutorial to create a login spring application

---
+++ Experiments/aidans_experiments/spring-boot-login/src/main/java/repository/RoleRepository.java
@@ -0,0 +1,7 @@
+package auth.repository;
+
+import auth.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long>{
+}

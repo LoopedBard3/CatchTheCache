ac03f8f11b5153519ad0a59fee149dba65a5f570
chloegolter <chloegolter@gmail.com>
2019-01-28 11:04:09 -0600

    Still learning

--- Experiments/anastasias_experiments/ASPractice/app/src/main/java/com/example/experimentallogin/LoginActivity.java
+++ Experiments/anastasias_experiments/ASPractice/app/src/main/java/com/example/experimentallogin/LoginActivity.java
@@ -53,6 +53,7 @@ import static android.Manifest.permission.READ_CONTACTS;
 
 /**
  * A login screen that offers login via email/password.
+ * Chloe is experimenting here
  */
 public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<Cursor> {
 

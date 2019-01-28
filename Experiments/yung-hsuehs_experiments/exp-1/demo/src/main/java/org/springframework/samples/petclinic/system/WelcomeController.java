08e10ef539a49f116bf12221d311f505c3cb2cfa
ann3431 <ann3431@hotmail.com>
2019-01-27 23:33:51 -0600

    Updated exp-1
    Modified some files to get petclinic working

--- Experiments/yung-hsuehs_experiments/exp-1/demo/src/main/java/org/springframework/samples/petclinic/system/WelcomeController.java
+++ Experiments/yung-hsuehs_experiments/exp-1/demo/src/main/java/org/springframework/samples/petclinic/system/WelcomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 class WelcomeController {
 
-    @GetMapping("/")
+    @GetMapping("/test")
     public String welcome() {
         return "welcome";
     }

0819dcf6f590626cf6d2d66fd4ff3e4ea9c97126
Parker Bibus <pbibus@iastate.edu>
2019-02-02 15:42:53 -0600

    Added models folder and created request class
    Added models folder and created request class

---
+++ Frontend/CatchTheCacheAndroid/app/src/main/java/edu/iastate/cs309/jr2/catchthecacheandroid/models/UserLoginRequest.java
@@ -0,0 +1,10 @@
+package edu.iastate.cs309.jr2.catchthecacheandroid.models;
+
+public class UserLoginRequest {
+    private String Username, Password;
+
+    public UserLoginRequest(String User, String Pass){
+        Username = User;
+        Password = Pass;
+    }
+}

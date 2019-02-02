1cc5ea8bf818805573cbd1ea74c6442904452f74
aidans <aidans@iastate.edu>
2019-02-02 15:24:51 -0600

    Created base backend spring project

---
+++ Backend/CatchTheCacheServer/src/main/java/edu/iastate/cs309/jr2/CatchTheCacheServer/CatchTheCacheServerApplication.java
@@ -0,0 +1,14 @@
+package edu.iastate.cs309.jr2.CatchTheCacheServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatchTheCacheServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatchTheCacheServerApplication.class, args);
+	}
+
+}
+

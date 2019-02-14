14dfd864db4566ff007553063c18128d6b97195f
LoopedBard3 <pbibus@hotmail.com>
2019-02-14 15:05:16 -0600

    Matched the cache model to the server version.
    Matched the cache model to the server version. Also made it so the obtained caches should be loaded in.

--- Frontend/CatchTheCacheAndroid/app/src/main/java/edu/iastate/cs309/jr2/catchthecacheandroid/CacheListActivity.java
+++ Frontend/CatchTheCacheAndroid/app/src/main/java/edu/iastate/cs309/jr2/catchthecacheandroid/CacheListActivity.java
@@ -1,12 +1,9 @@
 package edu.iastate.cs309.jr2.catchthecacheandroid;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -21,15 +18,14 @@ import org.json.JSONObject;
 import java.util.ArrayList;
 
 import edu.iastate.cs309.jr2.catchthecacheandroid.adapters.CacheListAdapter;
-import edu.iastate.cs309.jr2.catchthecacheandroid.models.cache_models.CacheIndividual;
+import edu.iastate.cs309.jr2.catchthecacheandroid.models.cache_models.Cache;
 import edu.iastate.cs309.jr2.catchthecacheandroid.models.cache_models.CacheListResponse;
-import edu.iastate.cs309.jr2.catchthecacheandroid.models.user_models.UserLoginResponse;
 
 public class CacheListActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager layoutManager;
-    private ArrayList<CacheIndividual> caches = new ArrayList<>();
+    private ArrayList<Cache> caches = new ArrayList<>();
     private RequestQueue queue;
     private Gson gson;
 
@@ -44,11 +40,12 @@ public class CacheListActivity extends AppCompatActivity {
         if(!getIntent().getExtras().getBoolean("ThroughServer")){
             addTestCaches();
         }else{
-            JsonObjectRequest requestObject = new JsonObjectRequest(Request.Method.GET, getString(R.string.access_url) + "caches/list",null,
+            JsonObjectRequest requestObject = new JsonObjectRequest(Request.Method.GET, getString(R.string.access_url) + "caches",null,
                     new Response.Listener<JSONObject>() {
                         @Override
                         public void onResponse(JSONObject response) {
-                            CacheListResponse respJson = gson.fromJson(response.toString(), CacheListResponse.class);
+                            CacheListResponse cachesList = gson.fromJson(response.toString(), CacheListResponse.class);
+                            caches.addAll(cachesList.getCacheList());
                         }
                     }, new Response.ErrorListener() {
                 @Override
@@ -71,23 +68,23 @@ public class CacheListActivity extends AppCompatActivity {
     }
 
     private void addTestCaches(){
-        caches.add(new CacheIndividual("test1", 123.124, 1241.124));
-        caches.add(new CacheIndividual("test2", 123.125, 1241.125));
-        caches.add(new CacheIndividual("test3", 123.126, 1241.126));
-        caches.add(new CacheIndividual("test4", 123.127, 1241.1278));
-        caches.add(new CacheIndividual("test5", 123.128, 1241.128));
-        caches.add(new CacheIndividual("test6", 123.129, 1241.129));
-        caches.add(new CacheIndividual("test7", 123.1, 1241.12));
-        caches.add(new CacheIndividual("test8", 123.16, 1241.13));
-        caches.add(new CacheIndividual("test9", 123.17, 1241.14));
-        caches.add(new CacheIndividual("test11", 123.124, 1241.124));
-        caches.add(new CacheIndividual("test12", 123.125, 1241.125));
-        caches.add(new CacheIndividual("test13", 123.126, 1241.126));
-        caches.add(new CacheIndividual("test14", 123.127, 1241.1278));
-        caches.add(new CacheIndividual("test15", 123.128, 1241.128));
-        caches.add(new CacheIndividual("test16", 123.129, 1241.129));
-        caches.add(new CacheIndividual("test17", 123.1, 1241.12));
-        caches.add(new CacheIndividual("test18", 123.16, 1241.13));
-        caches.add(new CacheIndividual("test19", 123.17, 1241.14));
+        caches.add(new Cache("test1", 123.124, 1241.124));
+        caches.add(new Cache("test2", 123.125, 1241.125));
+        caches.add(new Cache("test3", 123.126, 1241.126));
+        caches.add(new Cache("test4", 123.127, 1241.1278));
+        caches.add(new Cache("test5", 123.128, 1241.128));
+        caches.add(new Cache("test6", 123.129, 1241.129));
+        caches.add(new Cache("test7", 123.1, 1241.12));
+        caches.add(new Cache("test8", 123.16, 1241.13));
+        caches.add(new Cache("test9", 123.17, 1241.14));
+        caches.add(new Cache("test11", 123.124, 1241.124));
+        caches.add(new Cache("test12", 123.125, 1241.125));
+        caches.add(new Cache("test13", 123.126, 1241.126));
+        caches.add(new Cache("test14", 123.127, 1241.1278));
+        caches.add(new Cache("test15", 123.128, 1241.128));
+        caches.add(new Cache("test16", 123.129, 1241.129));
+        caches.add(new Cache("test17", 123.1, 1241.12));
+        caches.add(new Cache("test18", 123.16, 1241.13));
+        caches.add(new Cache("test19", 123.17, 1241.14));
     }
 }
--- Frontend/CatchTheCacheAndroid/app/src/main/java/edu/iastate/cs309/jr2/catchthecacheandroid/models/cache_models/CacheListRequest.java
+++ Frontend/CatchTheCacheAndroid/app/src/main/java/edu/iastate/cs309/jr2/catchthecacheandroid/models/cache_models/CacheListRequest.java
@@ -1,15 +1,15 @@
 package edu.iastate.cs309.jr2.catchthecacheandroid.models.cache_models;
 
 public class CacheListRequest {
-    private double player_longitude, player_latitude;
+    private double playerLongitude, playerLatitude;
 
-    public CacheListRequest(double player_longitude, double player_latitude) {
-        this.player_longitude = player_longitude;
-        this.player_latitude = player_latitude;
+    public CacheListRequest(double playerLongitude, double playerLatitude) {
+        this.playerLongitude = playerLongitude;
+        this.playerLatitude = playerLatitude;
     }
 
     public CacheListRequest() {
-        this.player_latitude = 42.02413;
-        this.player_longitude = -93.65069;
+        this.playerLatitude = 42.02413;
+        this.playerLongitude = -93.65069;
     }
 }

2b3bbb96ae268f590fcf659a33123edc817cfa95
aidans <aidans@iastate.edu>
2019-02-14 11:04:33 -0600

    Removed unused classes

--- Backend/CatchTheCacheServer/src/main/java/edu/iastate/cs309/jr2/CatchTheCacheServer/user/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package edu.iastate.cs309.jr2.CatchTheCacheServer.user;
-
-public class UserNotFoundException extends RuntimeException {
-	UserNotFoundException(Long id) {
-		super("Could not find user " + id);
-	}
-}

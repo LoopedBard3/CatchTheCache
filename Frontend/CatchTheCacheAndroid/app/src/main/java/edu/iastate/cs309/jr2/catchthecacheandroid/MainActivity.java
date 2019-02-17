9479695851edca47465ea4ee242eef0333db9ec1
chloegolter <chloegolter@gmail.com>
2019-02-17 15:20:51 -0600

    its Json and Gson?

--- Frontend/CatchTheCacheAndroid/app/src/main/java/edu/iastate/cs309/jr2/catchthecacheandroid/MainActivity.java
+++ Frontend/CatchTheCacheAndroid/app/src/main/java/edu/iastate/cs309/jr2/catchthecacheandroid/MainActivity.java
@@ -1,19 +1,43 @@
 package edu.iastate.cs309.jr2.catchthecacheandroid;
 
+import android.app.Notification;
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.widget.TextView;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+import android.widget.EditText;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+
+
+
 import edu.iastate.cs309.jr2.catchthecacheandroid.dummy.DummyContent;
 import edu.iastate.cs309.jr2.catchthecacheandroid.ui.main.MainFragment;
 
 public class MainActivity extends AppCompatActivity implements chatFragment.OnListFragmentInteractionListener {
+    private EditText mUserView;
+    private EditText mCacheIdView;
+    private EditText mIdView;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +53,107 @@ public class MainActivity extends AppCompatActivity implements chatFragment.OnLi
         configureBackButton();
         //configureRecyleButton();
         //configureCacheToggle();
+        checkNetworkConnection();
+        mUserView = findViewById(R.id.user);
+        mCacheIdView = findViewById(R.id.cacheId);
+        mIdView = findViewById(R.id.id);
+
+
     }
+    public boolean checkNetworkConnection() {
+        ConnectivityManager connMgr = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        boolean isConnected = false;
+        if (networkInfo != null && (isConnected = networkInfo.isConnected())) {
+            // show "Connected" & type of network "WIFI or MOBILE"
+
+
+
+        } else {
+            // show "Not Connected"
+
+        }
+
+        return isConnected;
+    }
+    private class HTTPAsyncTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+            // params comes from the execute() call: params[0] is the url.
+            try {
+                try {
+                    return HttpPost(urls[0]);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    return "Error!";
+                }
+            } catch (IOException e) {
+                return "Unable to retrieve web page. URL may be invalid.";
+            }
+        }
+        // onPostExecute displays the results of the AsyncTask.
+        @Override
+        protected void onPostExecute(String result) {
+
+        }
+    }
+
+
+    private String HttpPost(String myUrl) throws IOException, JSONException {
+        String result = "";
+
+        URL url = new URL(myUrl);
+
+        // 1. create HttpURLConnection
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("POST");
+        conn.setRequestProperty("Content-Type", "application/json; charset=utf-8");
+
+        // 2. build JSON object
+        JSONObject jsonObject = buidJsonObject();
+
+        // 3. add JSON content to POST request body
+        setPostRequestContent(conn, jsonObject);
+
+        // 4. make POST request to the given URL
+        conn.connect();
+
+        // 5. return response message
+        return conn.getResponseMessage()+"";
+
+    }
+    //TODO change to fit our Stuff
+    private JSONObject buidJsonObject() throws JSONException {
+
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.accumulate("cacheId", mCacheIdView.getText().toString());
+        jsonObject.accumulate("id",  mIdView.getText().toString());
+        jsonObject.accumulate("user",  mUserView.getText().toString());
+
+        return jsonObject;
+    }
+
+    private void setPostRequestContent(HttpURLConnection conn,
+                                       JSONObject jsonObject) throws IOException {
+
+        OutputStream os = conn.getOutputStream();
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(os, "UTF-8"));
+        writer.write(jsonObject.toString());
+        Log.i(MainActivity.class.toString(), jsonObject.toString());
+        writer.flush();
+        writer.close();
+        os.close();
+    }
+
+
+
+
+
+
+
+
     private void configureBackButton(){
         Button nextbutton = (Button) findViewById(R.id.back);
         nextbutton.setOnClickListener(new View.OnClickListener() {

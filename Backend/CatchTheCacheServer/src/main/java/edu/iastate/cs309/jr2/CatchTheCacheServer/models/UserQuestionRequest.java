f4371e9e418d29e61dca61a5fb0f6cb80b069b88
aidans <aidans@iastate.edu>
2019-02-26 20:13:58 -0600

    Updated UserQuestion Models

--- Backend/CatchTheCacheServer/src/main/java/edu/iastate/cs309/jr2/CatchTheCacheServer/models/UserQuestionRequest.java
+++ Backend/CatchTheCacheServer/src/main/java/edu/iastate/cs309/jr2/CatchTheCacheServer/models/UserQuestionRequest.java
@@ -3,6 +3,8 @@ package edu.iastate.cs309.jr2.CatchTheCacheServer.models;
 public class UserQuestionRequest {
 	private String username;
 
+	public UserQuestionRequest() {}
+	
 	public UserQuestionRequest(String u) {
 		this.username = u;
 	}

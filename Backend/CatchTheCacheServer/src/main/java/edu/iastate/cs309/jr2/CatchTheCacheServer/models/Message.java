aaae4b3c1fbb9387e25e726c933e891810debe2e
ann3431 <ann3431@hotmail.com>
2019-03-09 21:33:22 -0600

    Update on Message class
    Added type of message, and moved to models package

---
+++ Backend/CatchTheCacheServer/src/main/java/edu/iastate/cs309/jr2/CatchTheCacheServer/models/Message.java
@@ -0,0 +1,40 @@
+package edu.iastate.cs309.jr2.CatchTheCacheServer.models;
+
+public class Message {
+	 
+	private MessageType type;
+    private String content;
+    private String sender;
+
+    public enum MessageType {
+        CHAT,
+        JOIN,
+        LEAVE
+    }
+
+    public MessageType getType() {
+        return type;
+    }
+
+    public void setType(MessageType type) {
+        this.type = type;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+ 
+    
+}

8dff115ecdaaf68b7ef3526a6a31d995168bbcc3
chloegolter <chloegolter@gmail.com>
2019-04-10 13:55:52 -0500

    creating chat adapter

---
+++ Frontend/CatchTheCacheAndroid/app/src/main/java/edu/iastate/cs309/jr2/catchthecacheandroid/adapters/ChatAdapter.java
@@ -0,0 +1,13 @@
+package edu.iastate.cs309.jr2.catchthecacheandroid.adapters;
+
+import java.util.ArrayList;
+
+import edu.iastate.cs309.jr2.catchthecacheandroid.models.chat_models.Message;
+import edu.iastate.cs309.jr2.catchthecacheandroid.models.user_models.User;
+
+public class ChatAdapter {
+    private ArrayList<Message> mChats;
+
+    private User user;
+
+}

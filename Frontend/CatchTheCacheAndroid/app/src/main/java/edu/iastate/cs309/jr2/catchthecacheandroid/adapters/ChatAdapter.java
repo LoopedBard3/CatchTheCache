d73c15bc5ffe0088f78c89f0b3d547bd37c5c172
chloegolter <chloegolter@gmail.com>
2019-04-11 12:49:03 -0500

    working on chat adapter

--- Frontend/CatchTheCacheAndroid/app/src/main/java/edu/iastate/cs309/jr2/catchthecacheandroid/adapters/ChatAdapter.java
+++ Frontend/CatchTheCacheAndroid/app/src/main/java/edu/iastate/cs309/jr2/catchthecacheandroid/adapters/ChatAdapter.java
@@ -1,13 +1,63 @@
 package edu.iastate.cs309.jr2.catchthecacheandroid.adapters;
 
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
 import java.util.ArrayList;
 
+import edu.iastate.cs309.jr2.catchthecacheandroid.R;
 import edu.iastate.cs309.jr2.catchthecacheandroid.models.chat_models.Message;
 import edu.iastate.cs309.jr2.catchthecacheandroid.models.user_models.User;
 
-public class ChatAdapter {
+public class ChatAdapter  extends RecyclerView.Adapter<ChatAdapter.ChatViewHolder> {
     private ArrayList<Message> mChats;
-
     private User user;
+
+
+    @Override
+    public ChatAdapter.ChatViewHolder onCreateViewHolder(final ViewGroup parent,
+                                                                   int viewType) {
+        // create a new view
+        View v = (View) LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.cache_chat_view_list, parent, false);
+        final ChatAdapter.ChatViewHolder vh = new ChatAdapter.ChatViewHolder(v);
+        //TODO make a chat_view_list
+        return vh;
+
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ChatAdapter.ChatViewHolder chatViewHolder, int i) {
+        Message msg = mChats.get(i);
+        TextView name = chatViewHolder.msg_creator;
+        name.setText(msg.getSender());
+
+        TextView text = chatViewHolder.msg;
+        text.setText(msg.getText());
+    }
+
+
+    public ChatAdapter(ArrayList<Message> myDataset, User usr) {
+        mChats = myDataset;
+        user = usr;
+    }
+
+    @Override
+    public int getItemCount() {
+        return mChats.size();
+    }
+
+    public static class ChatViewHolder extends RecyclerView.ViewHolder {
+        public TextView msg_creator, msg;
+        public ChatViewHolder(View v) {
+            super(v);
+            msg_creator = v.findViewById(R.id.msg_creator);
+            msg = v.findViewById(R.id.msg);
+        }
+    }
 
 }

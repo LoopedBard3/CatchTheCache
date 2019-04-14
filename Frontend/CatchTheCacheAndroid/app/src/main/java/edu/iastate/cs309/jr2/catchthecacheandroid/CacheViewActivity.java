c3d545e6a954192a07635e16b351760cb6fdbfc9
Parker Bibus <pbibus@hotmail.com>
2019-04-14 16:09:36 -0500

    Made it so only admins view their goal point while moderators cannot. Both are also able to skip finding the cache to view a cache chat. This also means that regular users have to do the cache in order to get into the cache chat.

--- Frontend/CatchTheCacheAndroid/app/src/main/java/edu/iastate/cs309/jr2/catchthecacheandroid/CacheViewActivity.java
+++ Frontend/CatchTheCacheAndroid/app/src/main/java/edu/iastate/cs309/jr2/catchthecacheandroid/CacheViewActivity.java
@@ -1,15 +1,11 @@
 package edu.iastate.cs309.jr2.catchthecacheandroid;
 
 import android.app.AlertDialog;
-import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
@@ -21,21 +17,16 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
-import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.Circle;
 import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 
-import org.json.JSONException;
 
 import edu.iastate.cs309.jr2.catchthecacheandroid.models.cache_models.Cache;
 import edu.iastate.cs309.jr2.catchthecacheandroid.models.user_models.User;
@@ -88,6 +79,8 @@ public class CacheViewActivity extends AppCompatActivity implements OnMapReadyCa
                 openChatOptionDialog();
             }
         });
+        if(usr.getAuthority() > 0) btn2.setVisibility(View.VISIBLE);
+        else btn2.setVisibility(View.GONE);
         getSupportActionBar().setTitle(cache.name);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 

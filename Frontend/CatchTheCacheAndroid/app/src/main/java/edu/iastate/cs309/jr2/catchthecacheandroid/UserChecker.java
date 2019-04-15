13b6e7d8d604ae31e94e9476422cf17e2f115d3a
LoopedBard3 <pbibus@hotmail.com>
2019-04-15 14:14:06 -0500

    Added javadocs for the UserChecker class.

--- Frontend/CatchTheCacheAndroid/app/src/main/java/edu/iastate/cs309/jr2/catchthecacheandroid/UserChecker.java
+++ Frontend/CatchTheCacheAndroid/app/src/main/java/edu/iastate/cs309/jr2/catchthecacheandroid/UserChecker.java
@@ -1,12 +1,29 @@
 package edu.iastate.cs309.jr2.catchthecacheandroid;
 
+
+/**
+ *  A class that holds the username and password validation methods.
+ * @author Parker Bibus
+ */
 public class UserChecker {
 
+    /**
+     * Checks if the specified username meets the username expectations
+     * @param username
+     * @return whether or not a username is valid
+     * @author Parker Bibus
+     */
     public boolean isUsernameValid(String username) {
         //https://stackoverflow.com/questions/12018245/regular-expression-to-validate-username
         return username.matches("^(?=.{3,}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$");
     }
 
+    /**
+     * Checks if the specified password meets the password expectations
+     * @param password
+     * @return whether or not a password is valid
+     * @author Parker Bibus
+     */
     public boolean isPasswordValid(String password) {
         //https://stackoverflow.com/questions/3802192/regexp-java-for-password-validation
         return password.matches("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=\\S+$).{4,}$");
